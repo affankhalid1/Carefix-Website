dfc4f7328de3c3b3e611322fcfc4f8ee1ef5a4fb
affankhalid1 <affankhalid560@gmail.com>
2024-07-18 12:43:51 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyRJQhG7okQv2AY21TD9OIxaMpx8SoVT-thEJ0tzxQTd3ZXXfjmefYomUs4ivfonzWN1A/exec'
+
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    fetch(scriptURL, {method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thank you! your form is submitted successfully"))
+    .then(() => {window.location.reload(); })
+    .catch(error => console.error('Error!', error.message))
+})
